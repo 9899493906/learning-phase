a6128844420239ad8efe383a8172986e1dd99165
Navdeep Mann <navdeep.mann@ballisticlearning.com>
2018-10-03 11:16:58 +0530

    new function added

--- index.php
+++ index.php
@@ -4,4 +4,7 @@ function school_preprocess_block(&$variables) {
     $variables['title_attributes_array']['class'][] = 'title';
     $variables['classes_array'][] = 'clearfix';
 }
-dsdfadf
+function school_preprocess_comment(&$variables) {
+
+    $variables['submitted'] = $variables['created'] . '—' . $variables['author'];
+}
